41cb990f1a05b8f842a8964fee15e0a1942224f0
Sankurabothu Prasanth <psankurabothu@corp.miraclesoft.com>
2022-12-22 15:19:02 +0530

    first commit

---
+++ miracle_task1-master/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pro';
+  cities: any[];
+
+  selectedCity: any;
+  constructor() {
+    this.cities = [
+      {name: 'New York', code: 'NY'},
+      {name: 'Rome', code: 'RM'},
+      {name: 'London', code: 'LDN'},
+      {name: 'Istanbul', code: 'IST'},
+      {name: 'Paris', code: 'PRS'}
+  ];
+  }
+
+
+}
+ 
